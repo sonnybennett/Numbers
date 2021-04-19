b44b747fbf76b97009382e240f349a4aed87e1fc
Sonny Bennett <RC0042859@student.riversidecollege.ac.uk>
2021-04-19 15:16:04 +0100

    added pi an c

--- Numbers/Form1.cs
+++ Numbers/Form1.cs
@@ -25,9 +25,20 @@ namespace Numbers
             richTextBox1.AppendText(d1.ToString("0.0E0") + "\n");
             richTextBox1.AppendText(d1.ToString("0.00E00") + "\n");
 
+            double pi= 3.141592653589793;
+            double c= 299792458;
+
+            richTextBox1.AppendText(pi.ToString("N15") + "\n");
+            richTextBox1.AppendText(pi.ToString("N5") + "\n");
+            richTextBox1.AppendText(pi.ToString("N15") + "\n");
+
+            richTextBox1.AppendText(c.ToString() + "\n");
+            richTextBox1.AppendText(c.ToString("N0") + "\n");
+            richTextBox1.AppendText(c.ToString("0.000000000000000") + "\n");
+            richTextBox1.AppendText(c.ToString("0.000E0") + "\n");
+
         }
-      
-    
+           
     private void RichTextBox1_TextChanged(object sender, EventArgs e)
         {
 
